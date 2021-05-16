bd3dcaf0885dfa9ab141ce799a41af90f6b80c2e
sandesh <sundesz@gmail.com>
2021-05-17 00:39:47 +0300

    Initalize project

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Sandesh')
+})
+
+app.get('/api/dates', (req, res) => {
+    res.json(new Date())
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`)
+})
