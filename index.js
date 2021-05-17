beeffbe89f2ae59d8d3f5ff8a653d6343354c602
sandesh <sundesz@gmail.com>
2021-05-17 23:22:00 +0300

    Add frontend

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
+app.use(express.static('build'))
 
 let notes = [{
     id: 1,
@@ -21,6 +25,38 @@ let notes = [{
 }]
 
 
+let persons = [{
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+},
+{
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+},
+{
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+},
+{
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+},
+{
+    "name": "Sandesh Hyoju",
+    "number": "0452177225",
+    "id": 5
+},
+{
+    "name": "Sajani Karmacharya",
+    "number": "1",
+    "id": 6
+}]
+
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello Sandesh')
 })
@@ -32,6 +68,10 @@ app.get('/api/dates', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
     res.json(notes)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
 
 const PORT = process.env.PORT || 3001
